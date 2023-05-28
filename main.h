8a79236d35b755bdb123660fa7958b941ab26075
Gem Kalu <kalublessingnike@gmail.com>
2023-05-28 16:06:32 -0700

    README update & header file

---
+++ main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Standard library files imported */
+
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdio.h>
+
+/* Protoypes of functions */
+
+int _putchar(char z);
+int _printf(const char *format, ...);
+int _put_s(char *string);
+int _put_int(int integer);
+int  _my_puts(char *string);
+void  _get_int(int num);
+
+#endif /* MAIN_H */
