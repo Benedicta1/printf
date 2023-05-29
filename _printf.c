6795b357e2928084a4206052c1213c9e4d451dc1
Gem Kalu <kalublessingnike@gmail.com>
2023-05-29 06:42:43 -0700

    _printf function defining format specifiers c, s, and %

---
+++ _printf.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * _printf - This prints variables using format specifers
+ * @format: This is a character string)
+ * Return: The number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+	unsigned int  x = 0, r_value = 0;
+	va_list args;
+
+	va_start(args, format);
+
+	for ( ; format[x] != '\0' ; h++)
+	{
+		if (format[x] != '%')
+		{
+			_putchar(format[x]);
+		}
+		else if (format[x + 1] == 'c')
+		{
+			_putchar(va_arg(args, int));
+			x++;
+		}
+		else if (format[x + 1] == 's')
+		{
+			int my_val = _put_s(va_arg(args, char*));
+
+			x++;
+			r_value += (my_val - 1);
+		}
+		else if (format[x + 1] == '%')
+		{
+			_putchar('%');
+			x++;
+		}
+		my_value += 1;
+	}
+	return (r_value);
+}
